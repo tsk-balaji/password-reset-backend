b9f3b5d154bd2701c23f86f85db3bffba9a327a6
tsk-balaji <tskbalaji15@gmail.com>
2024-11-07 06:25:36 +0530

    cors

--- index.js
+++ index.js
@@ -4,39 +4,28 @@ const initialise_Mongo_Connectivity = require("./database/connection.mongodb");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const app = express();
 
+// CORS options
 const corsOptions = {
   origin: "https://password-reset-tsk.netlify.app",
   optionsSuccessStatus: 200, // For legacy browser support
 };
 
+// Apply CORS middleware with options
 app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://password-reset-tsk.netlify.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-
 
 // Connect to MongoDB
 initialise_Mongo_Connectivity();
 
-const app = express();
-
+// Middleware for parsing JSON
 app.use(bodyParser.json());
-app.use(cors());
 
-const PORT = process.env.PORT || 3000; // Change the port number here
+// Routes
+app.use("/", require("./pages/forgotpassword/forgotpassword.controller"));
 
 // Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("WebService is Live on port :", PORT);
+  console.log("WebService is Live on port:", PORT);
 });
-
-app.use("/", require("./pages/forgotpassword/forgotpassword.controller"));
