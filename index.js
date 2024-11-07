fc568be6b67ac8d1b926c82e5d19e1e54235f211
tsk-balaji <tskbalaji15@gmail.com>
2024-11-07 06:16:41 +0530

    cors changes

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const initialise_Mongo_Connectivity = require("./database/connection.mongodb");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+
+const corsOptions = {
+  origin: "https://password-reset-tsk.netlify.app",
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://password-reset-tsk.netlify.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
+
 // Connect to MongoDB
 initialise_Mongo_Connectivity();
 
